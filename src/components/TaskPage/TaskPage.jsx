ab1cf945272969337ac032387eed1026e119caea
Selam Alem <selamtalem@gmail.com>
2021-12-30 16:56:15 -0600

    Made Clients Visible on Home page

---
+++ src/components/TaskPage/TaskPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+
+function TaskPage() {
+
+  return(
+      <h1> Task List: </h1>
+  )
+
+    
+
+
+
+}
+
+export default TaskPage;
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import AddNewClientButton from '../AddNewClientButton/AddNewClientButton';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import { useEffect, useState } from 'react';
+
+
 
 function UserPage() {
+  const dispatch = useDispatch();
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const client = useSelector((store) => store.client)
+
   return (
     <div className="container">
       <h2>Welcome Team, {user.team_name}!</h2>
+      <h3>Here are your clients:</h3>
+      <ul>
+      {client.map((clientInfo) => {
+        return (
+          <li key={clientInfo.id}> {clientInfo.user_id === user.id && clientInfo.name}
+          { clientInfo.user_id === user.id }</li> //keeps log of the user ID who added client in our Database
+        )
+      })}
+    </ul>
+
       <AddNewClientButton className="btn"  />
       {/* <p>You are caring for: {client.name}</p> */}
       <LogOutButton className="btn" />
