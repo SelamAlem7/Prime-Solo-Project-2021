d789d3a3a78f1e2aa2a202645959c0493b46a3da
Selam Alem <selamtalem@gmail.com>
2022-01-08 17:50:27 -0600

    edited database file

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
--- DATABASE is called "solo_project"
+-- DATABASE is called "prime_app"
 -- USER is a reserved keyword with Postgres
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
