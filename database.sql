7af253520a04ccc954fa616d9cd1a47d74cce1c4
Selam Alem <selamtalem@gmail.com>
2022-01-10 21:27:09 -0600

    Updated database file

--- database.sql
+++ database.sql
@@ -3,6 +3,10 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
+
+--"client_id" INT REFERENCES "client" NOT NULL,
+
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "team_name" VARCHAR (100) NOT NULL,
@@ -11,21 +15,25 @@ CREATE TABLE "user" (
 );
 
 CREATE TABLE "client" (
-    "id" SERIAL PRIMARY KEY,
+    "id" SERIAL NOT NULL PRIMARY KEY,
     "name" VARCHAR (100) NOT NULL,
-    "diagnosis_list" VARCHAR (3000),
     "user_id" INT REFERENCES "user"
 );
 
 CREATE TABLE "tasks" (
     "id" SERIAL PRIMARY KEY,
-    "client_id" INT REFERENCES "client",
     "task" VARCHAR (255) NOT NULL,
     "completed_by" VARCHAR (255),
-    "completed" VARCHAR (1)
+    "completed" VARCHAR (1),
+    "client_id" INT REFERENCES "client"
+    
 );
 
 
+--to drop tables
+DROP TABLE IF EXISTS "tasks";
+DROP TABLE IF EXISTS "client";
+DROP TABLE IF EXISTS "user";
 
 
 
